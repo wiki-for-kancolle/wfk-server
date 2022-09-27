604e7d9dea9843f8a02ec34601202cb2539f28d9
JayJayZJU <jayjayzju@gmail.com>
2022-09-27 15:45:52 +0800

    chore: add koa-bodyparser

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,7 @@
 import Koa from 'koa';
 import { getLogger, Logger } from 'log4js';
 import { v4 as uuidv4 } from 'uuid';
+import bodyParser from 'koa-bodyparser';
 import { ServerEnv } from '../utils/env';
 
 export class Server {
@@ -9,6 +10,7 @@ export class Server {
     constructor() {
         this.app = new Koa();
 
+        // trace id
         this.app.use(async (ctx, next) => {
             if (!ctx.logger) ctx.logger = getLogger();
             const traceId = ctx.get('X-Request-Id') || uuidv4();
@@ -17,6 +19,7 @@ export class Server {
             await next();
         });
 
+        // logger
         this.app.use(async (ctx, next) => {
             const startTime = Date.now();
             await next();
@@ -24,8 +27,15 @@ export class Server {
             ctx.logger.info('cost time: %dms', endTime - startTime);
         });
 
+        // body parser
+        this.app.use(
+            bodyParser({
+                jsonLimit: '100mb',
+            }),
+        );
+
         this.app.use(async ctx => {
-            ctx.body = ctx.body ?? 'Hello World';
+            ctx.body = ctx.request.body;
 
             const logger = ctx.logger as Logger;
             logger.debug(ctx.url, ctx.body);
