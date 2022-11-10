9928cb432f2b908c801dea62f0772f3e06cc3031
JayJayZJU <jayjayzju@gmail.com>
2022-11-10 10:10:06 +0800

    test: remove test files

--- src/server/xc.ts
+++
@@ -1,9 +0,0 @@
-import { Server } from '../utils/server';
-import { XcBill } from '../service/xc/bill';
-
-class XcServer extends Server {}
-
-const server = new XcServer();
-server.registServices([new XcBill()]);
-server.initialize();
-server.start();
